cb36eaeff9406a630c6873b5ba2b1020cffcb44a
jlacy <josephinalacy@gmail.com>
2023-08-08 21:21:51 -0500

    add-validation-annotations

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,17 +1,27 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+    @Size(max = 500, message = "Description is too long.")
     private String description;
 
-    public Event(String name, String description){
+    @Email(message = "Invalid Email, try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String email){
         this.name = name;
         this.description = description;
+        this.contactEmail = email;
         this.id = nextId;
         nextId++;
     }
@@ -30,6 +40,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
